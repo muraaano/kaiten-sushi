a35f52ffe197ed86d9387c837a4cb998c7c66c96
Masaki Murano <masaki.murano@livesense.co.jp>
2020-12-25 10:46:50 +0900

    debugging random Vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    println!("へい　らっしゃい！");
+    println!();
+
+    args = shuffle(args);
+
+    for (i, arg) in args.iter().enumerate() {
+        println!("{}皿目は、{}", i+1, arg);
+    }
+
+    println!();
+    println!("おあいそ！");
+}
+
+
+fn shuffle(args: Vec<String>) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    args.shuffle(&mut rng);
 }
